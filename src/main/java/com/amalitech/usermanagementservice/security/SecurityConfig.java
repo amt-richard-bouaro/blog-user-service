ff5331cd28ac285c4a8c809ef4b11ef3339e2b52
rbouaro-se <richard.bouaro@amalitech.com>
2024-12-21 12:54:39 +0000

    feat: add fetching user by id path to the whitelist

--- src/main/java/com/amalitech/usermanagementservice/security/SecurityConfig.java
+++ src/main/java/com/amalitech/usermanagementservice/security/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private static final String[] WHITE_LIST_URL = {"/", "/api/v1/auth/login","/api/v1/auth/password-reset", "/api/v1/users/register", "/api/v1/users/check-username", "/actuator/**", "/v2/api-docs", "/*/v3/api-docs", "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/swagger-ui/index.html",
+    private static final String[] WHITE_LIST_URL = {"/", "/api/v1/auth/login","/api/v1/auth/password-reset", "/api/v1/users/register", "/api/v1/users/check-username","/api/v1/users/{id}", "/actuator/**", "/v2/api-docs", "/*/v3/api-docs", "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/swagger-ui/index.html",
             "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html"};
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
