f3ffda1d5a96c7b9fd75464a45a47e967c9906f5
rbouaro-se <richard.bouaro@amalitech.com>
2024-12-29 03:01:58 +0000

    chore: add workflow to check test coverage before allow merge

--- src/test/java/com/amalitech/usermanagementservice/properties/EmailPropertiesTestEnvironmentTest.java
+++ src/test/java/com/amalitech/usermanagementservice/properties/EmailPropertiesTestEnvironmentTest.java
@@ -1,21 +1,20 @@
 package com.amalitech.usermanagementservice.properties;
 
+
+import com.amalitech.usermanagementservice.ConfigTest;
 import com.amalitech.usermanagementservice.config.properties.EmailPropertiesConfig;
-import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.ValidatorFactory;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import jakarta.validation.Validator;
 import java.util.Set;
@@ -27,33 +26,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 @Epic("Application Properties")
 @Feature("Email Properties Configuration Test")
-class EmailPropertiesConfigTest {
+final class EmailPropertiesConfigTest extends ConfigTest {
 
-    private final Logger logger = LoggerFactory.getLogger(EmailPropertiesConfigTest.class);
-
-    private Validator validator;
-
-    @Autowired
-    private Environment environment;
+    private static final Logger logger = LoggerFactory.getLogger(EmailPropertiesConfigTest.class);
 
     @Autowired
     private EmailPropertiesConfig emailPropertiesConfig;
 
-    @BeforeEach
-    void setUp() {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
         try(ValidatorFactory factory = Validation.buildDefaultValidatorFactory();){
             validator = factory.getValidator();
         }catch (Exception e){
             logger.debug(e.getMessage());
         }
-    }
-
-    @Test
-    @Story("Verify active profiles in the application")
-    @Description("This test verifies that the active profile is correctly set to 'test' during the test environment setup.")
-    void testActiveProfile() {
-        String[] activeProfiles = environment.getActiveProfiles();
-        assertArrayEquals(new String[]{"test"}, activeProfiles);
     }
 
     @Test
